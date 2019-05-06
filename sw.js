01ee81c462d01dd91172fd97b735c08ba442fecf
Jorge Soeiro <jorgemsoeiro@gmail.com>
2019-05-06 20:20:01 +0200

    Stage1

---
+++ sw.js
@@ -0,0 +1,63 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    '/',
+    './index.html',
+    './restaurant.html',
+    './data/restaurants.json',
+    './css/styles.css',
+    './css/responsive.css',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js',
+    './js/register_sw.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+];
+
+// Call Install Event
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Service Worker: Caching Files');
+            cache.addAll(cacheAssets);
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    // Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
